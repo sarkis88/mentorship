c68c2394e065192e20bf24a2f1cc36f1e94ffe7d
sarkis <sarkis.makarian@picsart.com>
2020-08-21 11:32:56 +0400

    new requirement with refactoring

--- src/main/kotlin/com/mentorcliq/mentorship/domain/Employee.kt
+++ src/main/kotlin/com/mentorcliq/mentorship/domain/Employee.kt
@@ -1,14 +1,7 @@
 package com.mentorcliq.mentorship.domain
 
 import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonValue
-import com.mentorcliq.mentorship.strategy.AGE_DIFF
-import com.mentorcliq.mentorship.strategy.AGE_WEIGHT
-import com.mentorcliq.mentorship.strategy.DIVISION_WEIGHT
-import com.mentorcliq.mentorship.strategy.OFFSET_WEIGHT
-import java.math.BigDecimal
-import kotlin.math.abs
 
 data class Employee(
         var name: String,
